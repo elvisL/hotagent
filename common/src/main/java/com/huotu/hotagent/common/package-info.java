76355312617c9a29fac9f02a6bc887bcad968462
allan <lqc_huli@163.com>
2016-01-22 16:04:05 +0800

    update admin module

---
+++ common/src/main/java/com/huotu/hotagent/common/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+/**
+ * 一些基本的工具类,或者静态变量
+ * Created by allan on 1/22/16.
+ */
+package com.huotu.hotagent.common;
