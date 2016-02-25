b776f1f432e3297359d2d11b15bdee864ef05b9f
cwb <815498030@qq.com>
2016-02-25 17:04:35 +0800

    agent edit page

--- agent/src/main/java/com/huotu/hotagent/agent/model/AgentStatistics.java
+++ agent/src/main/java/com/huotu/hotagent/agent/model/AgentStatistics.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 /**
  * Created by chendeyu on 2016/2/19.
- * 代理商统计
+ *
  */
 
 @Data
@@ -12,27 +12,27 @@ public class AgentStatistics {
 
 
     /**
-     * 下级代理商总数
+     *
      */
     private long agentNumWithLevel;
 
     /**
-     * 总代理费用
+     *
      */
     private double agentCosts;
 
     /**
-     * 总返佣
+     *
      */
     private double agentCommission;
 
     /**
-     * 账户余额
+     *
      */
     private double balance;
 
     /**
-     * 佣金余额
+     *
      */
     private double commission;
 
