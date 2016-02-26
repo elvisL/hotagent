2e4f81745c3f98a7ce7c88dd70f674894c97e422
cwb <815498030@qq.com>
2016-02-26 11:19:16 +0800

    分页起始页码算法改进

--- common/src/main/java/com/huotu/hotagent/common/utils/CommonUtils.java
+++ common/src/main/java/com/huotu/hotagent/common/utils/CommonUtils.java
@@ -8,15 +8,19 @@ public class CommonUtils {
 
     /**
      * 计算分页button的起始显示页码
-     * @param currentPage
-     * @param pageBtnNum
-     * @param totalPages
+     * @param currentPage 当前页
+     * @param pageBtnNum 页码button数量 奇数
+     * @param totalPages 总页数
      * @return
      */
     public static int calculateStartPageNo(int currentPage, int pageBtnNum, int totalPages) {
-        if(pageBtnNum == totalPages) {
+        int middleBtn = pageBtnNum/2 + 1;
+        if(currentPage<=middleBtn || pageBtnNum == totalPages) {
             return 1;
         }
-        return currentPage - pageBtnNum + 1 < 1 ? 1 : currentPage - pageBtnNum + 1;
+        if((totalPages-currentPage)<middleBtn){
+            return totalPages - pageBtnNum + 1;
+        }
+        return   currentPage + middleBtn - pageBtnNum;
     }
 }
