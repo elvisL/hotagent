d2acfc1c8248ebc069646596cc9cd29ea8d125f0
allan <lqc_huli@163.com>
2016-08-17 10:57:08 +0800

    代理的商品可以修改了

--- admin/src/main/java/com/huotu/hotagent/admin/controller/product/ProductController.java
+++ admin/src/main/java/com/huotu/hotagent/admin/controller/product/ProductController.java
@@ -1,8 +1,18 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.hotagent.admin.controller.product;
 
-import com.huotu.hotagent.admin.model.ProductTreeModel;
 import com.huotu.hotagent.common.constant.ApiResult;
 import com.huotu.hotagent.common.constant.ResultCodeEnum;
+import com.huotu.hotagent.common.ienum.EnumHelper;
+import com.huotu.hotagent.service.common.ProductType;
 import com.huotu.hotagent.service.entity.product.Price;
 import com.huotu.hotagent.service.entity.product.Product;
 import com.huotu.hotagent.service.service.product.PriceService;
@@ -13,11 +23,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,7 +48,9 @@ public class ProductController {
     @PreAuthorize("hasAnyAuthority('MANAGER_ROOT','MANAGER_PRODUCT')")
     public String showProducts(Model model) throws Exception {
         List<Product> products = productService.findSubs();
-        model.addAttribute("products",products);
+
+        model.addAttribute("products", products);
+        model.addAttribute("productTypes", ProductType.values());
         return "product/product_list";
     }
 
@@ -66,6 +79,7 @@ public class ProductController {
 
     /**
      * 修改产品价格
+     *
      * @param id
      * @param basePrice
      * @return
@@ -73,13 +87,13 @@ public class ProductController {
     @RequestMapping("editProductPrice")
     @ResponseBody
     @Transactional
-    public ApiResult editProductPrice(Long id,Double basePrice) {
-        if(id == null || basePrice == null) {
+    public ApiResult editProductPrice(Long id, Double basePrice) {
+        if (id == null || basePrice == null) {
             return ApiResult.resultWith(ResultCodeEnum.DATA_NULL);
         }
         Product product = productService.findOne(id);
         List<Price> prices = priceService.findByProduct(product);
-        for(Price price : prices) {
+        for (Price price : prices) {
             price.setPrice(basePrice);
             priceService.save(price);
         }
@@ -101,10 +115,36 @@ public class ProductController {
             return ApiResult.resultWith(ResultCodeEnum.DATA_NULL);
         }
         product = productService.update(product);
-        if(product != null){
+        if (product != null) {
             return ApiResult.resultWith(ResultCodeEnum.SUCCESS);
-        }else{
+        } else {
             return ApiResult.resultWith(ResultCodeEnum.SAVE_DATA_ERROR);
         }
+    }
+
+    @RequestMapping(value = "/editProduct", method = RequestMethod.POST)
+    @PreAuthorize("hasAnyAuthority('MANAGER_ROOT','MANAGER_PRODUCT')")
+    @ResponseBody
+    public ApiResult editProduct(String name, int productType, long id, String productDesc, double basePrice) {
+        Product product;
+        if (id > 0) {
+            product = productService.findOne(id);
+
+            //修改价格
+            List<Price> prices = priceService.findByProduct(product);
+            for (Price price : prices) {
+                price.setPrice(basePrice);
+                priceService.save(price);
+            }
+
+        } else {
+            product = new Product();
+        }
+        product.setName(name);
+        product.setProductType(EnumHelper.getEnumType(ProductType.class, productType));
+        product.setBasePrice(basePrice);
+        product.setProductDesc(productDesc);
+        productService.save(product);
+        return ApiResult.resultWith(ResultCodeEnum.SUCCESS);
     }
 }
